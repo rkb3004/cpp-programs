7c53df91396dcf98dd4d84e10611c5f9a5fbf353
rkb1930 <iamrohankumarbehera@gmail.com>
2025-02-04 22:26:36 +0530

    sum

---
+++ basics/sum.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    int a,b,c;
+    cin>>n;
+    for(int i=0; i<n;i++){
+        cin>>a>>b>>c;
+        if(a+b==c || a+c==b ||b+c==a)
+            cout<<"YES"<<endl;
+        else
+            cout<<"NO"<<endl;
+    }
+    
+    
+}
