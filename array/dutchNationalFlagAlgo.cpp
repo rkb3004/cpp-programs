0a3df93671d43f1e8344092e24527d2583b18494
rkb1930 <iamrohankumarbehera@gmail.com>
2025-03-03 21:55:58 +0530

    dnf

---
+++ array/dutchNationalFlagAlgo.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void dutchNationalFlagAlgo(int arr[],int n){
+    int low=0;
+    int mid=0;
+    int high=n-1;
+    while(mid<=high){
+        if(arr[mid]==0){
+            swap(arr[low],arr[mid]);
+            low++;
+            mid++;
+        }
+        else if(arr[mid]==1){
+            mid++;
+        }
+        else{
+            swap(arr[mid],arr[high]);
+            high--;
+        }
+    }
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
