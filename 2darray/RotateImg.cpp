cbabfe8e8a7a81eb5af89bd9813648b3f4779adc
rkb1930 <iamrohankumarbehera@gmail.com>
2025-03-21 18:44:54 +0530

    rotate

---
+++ 2darray/RotateImg.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+  void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+
+        //transpose 
+        for(int =0;i<n;i++){
+            for(int j=0;j<i;j++){
+                matrix[i][j]=matrix[j][i]
+            }
+        }
+    }
+int main(){
+    
+}
