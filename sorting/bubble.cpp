76a6a4013a0bb428abdff43c31045b9d00675b94
rkb1930 <iamrohankumarbehera@gmail.com>
2025-05-11 22:54:39 +0530

    swap

--- sorting/bubble.cpp
+++ sorting/bubble.cpp
@@ -2,18 +2,24 @@
 using namespace std;
 
 void bubbleSort(int nums[], int n){
+    bool swapped= false;
     for(int i=n-1 ; i>=0; i--){
         for(int j=0; j<=i-1; j++){
             if(nums[j]>nums[j+1]){
                 swap(nums[j], nums[j+1]);
+                swapped=true;
             }
+        } 
+        if(swapped==false){
+            break;
         }
+        cout<<"After pass "<<endl;
     }
 
 }
 
 int main(){
-    int nums[]={ 13,46,24,52,9};
+    int nums[]={1,2,3,4,5};
     int n=5;
     cout<<"Before sorting: ";
     for(int i=0; i<n; i++){
